54d85aeb7fa6fb40251ef06950b533c5d89919bf
Oldrick Weenink <o.weenink@hotmail.com>
2023-07-27 13:24:51 +0200

    Refactored TTT

--- ticTacToe/ttt.js
+++ ticTacToe/ttt.js
@@ -1,108 +1,94 @@
-const board = ['','','','','','','','',''];
-let isPlayerXTurn = null;
-let playerXState = new Set();
-let playerOState = new Set();
-let gameIsStarted = false;
-
-let outcome = null;
-
-const WINNING_SOLUTIONS = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-]
-
-
-const controlButtons = document.querySelectorAll(".control-button")
+const controlButtons = document.querySelectorAll(".control-button");
 const buttons = document.querySelectorAll(".field");
 const menu = document.querySelector(".menu");
-
+const WINNING_SOLUTIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+const board = ['', '', '', '', '', '', '', '', ''];
+let currentPlayer = null;
+let running = false;
 let whosTurnDisplay = null;
 
-// This sets up which player begins, and starts the game.
-controlButtons.forEach(btn => {
-    btn.onclick = (src) => {
-        let val = src.target.value;
-        isPlayerXTurn = val == 'x' ? true : false;
-        changeMenu();
-        gameIsStarted = true;
+setUpControlButtons();
+setUpBoard();
+
+function setUpControlButtons() {
+    controlButtons.forEach(btn => btn.addEventListener("click", startGame));
+}
+
+function startGame(){
+    currentPlayer = this.value;;
+    changeMenu();
+    running = true;
+}
+function setUpBoard(){
+    buttons.forEach(btn => btn.addEventListener("click", cellClicked));
+}
+
+function cellClicked() {
+    if (this.innerText == "" && running) {
+        makeAMove(this);
+        checkForEndGame();
     }
-})
+}
 
-
-// Making a move when a valid field is clicked.
-// The timeout is too give some time to the program to draw the last 'x' or 'y' before showing the alert and refreshing.
-buttons.forEach(btn => {
-    btn.onclick = (src) => {
-        if (src.target.innerText == "" && gameIsStarted) {
-            makeAMove(src);
-            setTimeout(() => {
-                checkForEndGame();
-                if (outcome != null) {
-                    handleEndGame();
-                }
-              }, 10);
-        } 
-    }
-})
-
-// Once the beginning player is selected, remove the buttons and add a display that show's who's turn it is.
 function changeMenu() {
     controlButtons.forEach(btn => {
         btn.style.display = "none";
     })
+    menu.appendChild(getDisplay());
+}
+
+function changePlayer() {
+    currentPlayer = (currentPlayer == 'x') ? 'o' : 'x';
+    whosTurnDisplay.innerText = `${currentPlayer}'s turn`;
+}
+
+function makeAMove(btn) {
+    let position = btn.value;
+    board[position] = currentPlayer;
+    btn.innerText = currentPlayer;
+}
+
+function checkForEndGame() {
+    for (const solution of WINNING_SOLUTIONS) {
+        let a = board[solution[0]];
+        let b = board[solution[1]];
+        let c = board[solution[2]];
+        
+        if(!(a == "") && (a == b && b == c)){
+            handleEndGame(false);
+            return;
+        }
+    } 
+    if (!board.includes("")) {
+        handleEndGame(true); 
+    } else {
+        changePlayer();
+    }
+}
+
+function handleEndGame(isDraw) {
+    message = isDraw ? "Draw!" : `Player ${currentPlayer} wins!`;
+    setTimeout(()=> {
+        alert(message);
+        window.location.reload();
+    }, 100);
+}
+
+function getDisplay() {
     whosTurnDisplay = document.createElement('p');
-    whosTurnDisplay.innerText = (isPlayerXTurn ? "Turn: Player X" : "Turn: Player O");
+    whosTurnDisplay.innerText = `${currentPlayer}'s turn`;
     whosTurnDisplay.style.border = "2px solid white";
     whosTurnDisplay.style.padding = "5px";
     whosTurnDisplay.style.color = "white";
     whosTurnDisplay.style.color = "white";
-    menu.appendChild(whosTurnDisplay);
-}
-
-
-function makeAMove(src) {
-    let position = src.target.value;
-    currentPlayerSymbol = isPlayerXTurn ? 'x' : 'o';
-    board[position] = currentPlayerSymbol;
-    src.target.innerText = currentPlayerSymbol;
-    let set = isPlayerXTurn ? playerXState : playerOState;
-    set.add(parseInt(position));
-    isPlayerXTurn = !isPlayerXTurn;
-    whosTurnDisplay.innerText = isPlayerXTurn ? "Turn: Player X" : "Turn: Player O";
-}
-
-function checkForEndGame() {
-    console.log("Checking for end of game")
-    for (const solution of WINNING_SOLUTIONS) {
-        if (areAllElementsInSet(solution, playerXState)) {
-            outcome = 'x';
-            return;
-        } else if (areAllElementsInSet(solution, playerOState)) {
-            outcome = 'o';
-            return;
-        } else if (board.filter(e => e == "").length == 0) {
-            outcome = 'd';
-        }
-    }
-}
-
-function areAllElementsInSet(array, set) {
-    for (const element of array) {
-      if (!set.has(element)) {
-        return false;
-      }
-    }
-    return true;
-}
-
-function handleEndGame() {
-    let message = outcome == "x" ? "Player X wins!" : outcome == "o" ? "Player O wins!" : "Draw!";
-    alert(message);
-    window.location.reload();
+    return whosTurnDisplay;
 }
